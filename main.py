eae87a1f6cf8c1407c00bef4cbdda301164d4b4f
Ben Vince <ben_vince@btinternet.com>
2018-10-20 11:18:11 +0100

    Basic web server working

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)   
+
+@app.route('/')
+def index():
+    return "Hello world" 
+
+
+if __name__ == "__main__":
+   app.run('0.0.0.0', debug=True)
+
+
