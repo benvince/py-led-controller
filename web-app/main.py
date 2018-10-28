93678e5ccfe66849d3b93624a65135be26d8ead1
Ben Vince <ben_vince@btinternet.com>
2018-10-28 18:28:24 +0000

    Added light toggle URI

--- web-app/main.py
+++ web-app/main.py
@@ -22,14 +22,25 @@ def setup_zones():
     if(request.method == "POST"):
         for key, value in request.items():
             print(key)
-        
 
     #Get Zones from Database:-
-    cur.execute("SELECT * FROM ZONES")
+    cur.execute("SELECT zone_ip, zone_port FROM ZONES")
     data = cur.fetchall()
     
+    final_data = []
+
+    for ip, port in data:
+        try:
+            zone_config = json.loads(requests.get("http://{}:{}/api/get_config".format(ip,port)).text)
+            zone_name = zone_config['zone_name']
+            status = "ONLINE"
+        except:
+            zone_name = ""
+            status = "OFFLINE"
+
+        final_data.append([ip,port, zone_name, status])
     #Return zones.html template and pass zone data from db to it:-
-    return(render_template("zones.html", zones = data))
+    return(render_template("zones.html", zones = final_data))
 
 @app.route("/all_zone_configs")
 def all_zones():
@@ -43,7 +54,6 @@ def all_zones():
     data = cur.fetchall()
    
     html = ""
-        
 
     for zone_ip, zone_port in data:
         
@@ -56,12 +66,11 @@ def all_zones():
             html += "Zone Name: {}</br>".format(config['zone_name'])
             html += "No. of LEDs: {}<br>".format(config['num_leds'])
             html += "Pin of RPI for LEDS: {}<br>".format(config['pin_no']) 
+            html += "<i>{}</i></br>".format(config)
         except:
             html += "Can't connect to {}:{}<br>".format(zone_ip, zone_port)
 
     return(html) 
-
-
 
 if __name__ == "__main__":
    app.run('0.0.0.0', debug=True)
