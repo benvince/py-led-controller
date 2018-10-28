b8e6b02600b2262ab9beafbc9ce79d19d54da6be
Ben Vince <ben_vince@btinternet.com>
2018-10-28 15:44:02 +0000

    Client working with RPi.GPIO

--- client/client_app.py
+++ client/client_app.py
@@ -3,6 +3,8 @@
 import socket
 import json
 from flask import Flask, request
+import RPi.GPIO as GPIO
+
 
 PORT_NUMBER = 7878
 
@@ -13,7 +15,7 @@ class LED_Client():
     def __init__(self, config_file):
 
         self.load_config(config_file)
-   
+
     def load_config(self, config_file):
 
         with open(config_file, "r") as f:
@@ -30,7 +32,34 @@ app = Flask(__name__)
 def get_config():
     return(json.dumps(a.config))
 
+@app.route("/api/toggle_light")
+def toggle_light():
+    
+    pin = config.config['pin_no']
+    toggle_pin(pin)
+
+    return("Done")
+
+@app.route("/api/toggle_pin/<int:pin>", methods = ['GET'])
+def toggle_pin(pin):
+
+    #print("TOGGLE PIN {}".format(pin))
+    GPIO.output(pin,not bool(GPIO.input(pin))) ## Turn on GPIO pin 7
+   
+    return("Done")
+
 if __name__ == "__main__":
+    
+    config = LED_Client('config.json')
+
+    OUT_PINS = []
+    IN_PINS = []
+
+    OUT_PINS.append(config.config['pin_no'])
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(OUT_PINS, GPIO.OUT)
+    GPIO.setup(IN_PINS, GPIO.IN)
 
     app.run('0.0.0.0', port = PORT_NUMBER,  debug=True)
 
